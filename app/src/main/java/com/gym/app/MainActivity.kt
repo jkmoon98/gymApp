5c3f298ee26b79a6e0d261c5d57192fdaff6c5f6
jkmoon98 <jkmoon98@gmail.com>
2025-12-29 21:55:32 -0800

    Update MainActivity.kt

--- app/src/main/java/com/gym/app/MainActivity.kt
+++ app/src/main/java/com/gym/app/MainActivity.kt
@@ -2,16 +2,27 @@ package com.gym.app
 
 import android.app.Activity
 import android.os.Bundle
+import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 
 class MainActivity : Activity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val webView = WebView(this)
+
         webView.webViewClient = WebViewClient()
-        webView.settings.javaScriptEnabled = true
+
+        val settings: WebSettings = webView.settings
+        settings.javaScriptEnabled = true
+        settings.domStorageEnabled = true
+        settings.allowFileAccess = true
+        settings.allowContentAccess = true
+        settings.loadWithOverviewMode = true
+        settings.useWideViewPort = true
+
         webView.loadUrl("file:///android_asset/www/index.html")
 
         setContentView(webView)
